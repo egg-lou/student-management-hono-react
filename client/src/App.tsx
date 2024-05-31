3f0c3ef253930cad74a1ba5e47ef8327d0f34ab9
egglou <miguel.rafaellouie2003@gmail.com>
2024-05-31 21:48:27 +0800

    prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,30 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const fetchProfessors = async () => {
-      try {
-          const response = await fetch('http://localhost:3000/api/professors', {
-              method: 'GET',
-              headers: {
-                  'Content-Type': 'application/json',
-              },
-          })
-          const data = await response.json()
-          console.log(data);
-      } catch (error) {
-          console.error(error)
-      }
-  }
+    const fetchProfessors = async () => {
+        try {
+            const response = await fetch(
+                'http://localhost:3000/api/professors',
+                {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+            )
+            const data = await response.json()
+            console.log(data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
 
-  useEffect(() => {
+    useEffect(() => {
         fetchProfessors()
-  }, [])
+    }, [])
 
-  return (
-    <>
-    </>
-  )
+    return <></>
 }
 
 export default App
