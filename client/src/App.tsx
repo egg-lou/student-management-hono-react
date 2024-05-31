3180493ba7670aeea2d6d938019bf0eb8f76ca6f
egglou <miguel.rafaellouie2003@gmail.com>
2024-05-31 21:47:50 +0800

    init frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,30 @@
+import {useEffect, useState} from 'react'
+import './App.css'
+
+function App() {
+  const fetchProfessors = async () => {
+      try {
+          const response = await fetch('http://localhost:3000/api/professors', {
+              method: 'GET',
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+          })
+          const data = await response.json()
+          console.log(data);
+      } catch (error) {
+          console.error(error)
+      }
+  }
+
+  useEffect(() => {
+        fetchProfessors()
+  }, [])
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
